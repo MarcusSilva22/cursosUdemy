deb1e27f3f7d6ac9dc04c9f75f0d752ee3ad26e1
Marcus Silva <marcus.fms@gmail.com>
2018-07-04 16:22:22 -0300

    implementação do módulo categoria

--- cursomc/src/main/java/com/marcus/cursomc/cursomc/CursomcApplication.java
+++ cursomc/src/main/java/com/marcus/cursomc/cursomc/CursomcApplication.java
@@ -1,12 +1,30 @@
 package com.marcus.cursomc.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.marcus.cursomc.cursomc.domain.Categoria;
+import com.marcus.cursomc.cursomc.repositories.CategoriaRepository;
 
-	public static void main(String[] args) {
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+	@Autowired
+	private CategoriaRepository categoriaRepository; 
+	
+	public static void main(String[] args){
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 }
