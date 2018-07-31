4b2f42b26b27d3850d6ce2ee2574f508592fc75a
Marcus Silva <marcus.fms@gmail.com>
2018-07-31 11:54:13 -0300

    implementação de validações em categoria

--- cursomc/src/main/java/com/marcus/cursomc/cursomc/resources/CategoriaResource.java
+++ cursomc/src/main/java/com/marcus/cursomc/cursomc/resources/CategoriaResource.java
@@ -1,13 +1,24 @@
 package com.marcus.cursomc.cursomc.resources;
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.marcus.cursomc.cursomc.domain.Categoria;
+import com.marcus.cursomc.cursomc.dto.CategoriaDTO;
 import com.marcus.cursomc.cursomc.services.CategoriaService;
 
 @RestController
@@ -17,9 +28,51 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;
 	
+	@RequestMapping(value="/search", method=RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page, 
+			@RequestParam(value="limit", defaultValue="24") Integer limit, 
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC") String direction){
+		Page<Categoria> listCategorias = service.findPage(page, limit, orderBy, direction);
+		Page<CategoriaDTO> listDTO = listCategorias.map(categoria -> new CategoriaDTO(categoria));
+		return ResponseEntity.ok().body(listDTO);		
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> find(){
+		List<Categoria> listCategorias = service.findAlll();		
+		List<CategoriaDTO> listDTO = listCategorias.stream().map(categoria -> new CategoriaDTO(categoria)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);		
+	}
+	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id){
-		Categoria obj = service.buscar(id);
+		Categoria obj = service.find(id);
 		return ResponseEntity.ok(obj);
 	}
+	
+	@RequestMapping(method=RequestMethod.POST)	
+	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO categoriaDTO){
+		Categoria categoria = service.fromDTO(categoriaDTO);
+		categoria = service.insert(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(categoriaDTO.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+		
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)	
+	public ResponseEntity<Void> update(@RequestBody Categoria categoria, @PathVariable Integer id){
+		categoria = service.update(categoria);		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<?> delete(@PathVariable Integer id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	
 }
--- cursomc/src/main/java/com/marcus/cursomc/cursomc/services/CategoriaService.java
+++ cursomc/src/main/java/com/marcus/cursomc/cursomc/services/CategoriaService.java
@@ -1,11 +1,17 @@
 package com.marcus.cursomc.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.marcus.cursomc.cursomc.domain.Categoria;
+import com.marcus.cursomc.cursomc.dto.CategoriaDTO;
 import com.marcus.cursomc.cursomc.repositories.CategoriaRepository;
 import com.marcus.cursomc.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -14,10 +20,43 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id){
+	public List<Categoria> findAlll(){		
+		return repo.findAll();		
+	}
+	
+	public Categoria find(Integer id){
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public Categoria insert(Categoria categoria){
+		return repo.save(categoria);
+	}
+	
+	public Categoria update(Categoria categoria){
+		find(categoria.getId());
+		return repo.save(categoria);
+	}
+	
+	public void delete(Integer id){
+		find(id);
+		try{
+			repo.deleteById(id);	
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityViolationException("Não é possivel excluir uma categoria que possui produtos");
+		}
+		
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO categoriaDTO){
+		return new Categoria(categoriaDTO.getId(), categoriaDTO.getNome());
 	}
 
 }
